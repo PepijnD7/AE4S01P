9cfde14098ff3d66f71475ee2ecf8a83b2e2f3d3
Bavo2002 <59149478+Bavo2002@users.noreply.github.com>
2021-12-14 14:42:03 +0100

    stap 8-9

--- Enginesimu.py
+++ Enginesimu.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from Read_Data import linearize
 
 # Propellant data
 a = 0.005132
@@ -9,10 +10,11 @@ m_p = 0.758
 l_p = 0.107
 d_out = 0.0766
 d_port = 0.025
+d_t = 8.37 / 1000
+alpha = 15 * np.pi / 180
+
 
 # Regression Rate
-
-
 def regrate(P, a, n):
     r = a * (P/Pref)**n
     return r
@@ -43,7 +45,7 @@ plt.plot(P_list, r_list*10**3)
 plt.xlabel("Chamber Pressure (Pa)")
 plt.ylabel("Regression rate (mm/s)")
 plt.grid()
-plt.show()
+# plt.show()
 
 r_c = r_list[P_list == 100*10**6]
 t_list = np.arange(0, 10, 0.1)
@@ -67,8 +69,97 @@ fig.legend()
 plt.show()
 
 
+# Initial conditions
+P_c = 5 * Pref
+S = np.pi * l_p * d_port
+A_t = (d_t ** 2 * np.pi / 4)
+a *= (10 ** (-6)) ** n
+
+for _ in range(10):
+    c_star = linearize('Characteristic velocity_opt', P_c)
+    # print('c_star: ', c_star)
+
+    P_c = (c_star * rho_P * a * S / A_t)**(1 / (1-n))
+    # print('P_c: ', P_c)
+
+a = 0.005132
+r = regrate(P_c, a, n)
+m_dot = r * S * rho_P
+
+c_star = linearize('Characteristic velocity_opt', P_c)
+C_f = linearize('Thrust coefficient_opt', P_c)
+lamda = (1 + np.cos(alpha)) / 2
+T = C_f * P_c * A_t * lamda
+
+print(P_c, r, m_dot, T)
 
 
+# Simulation
+dt = 0.1
+
+m_list = []
+p_list = []
+T_list = []
+r_list = []
+I_list = [0]
+
+while d_port <= d_out:
+    S_burn = d_port * np.pi * l_p
+
+    a *= (10 ** (-6)) ** n
+    P_c = (c_star * rho_P * a * S_burn / A_t) ** (1 / (1 - n))
+
+    a = 0.005132
+    r_rate = regrate(P_c, a, n)
+
+    m_dot = rho_P * r_rate * S_burn
+    c_star = linearize('Characteristic velocity_opt', P_c)
+    T = m_dot * c_star * linearize('Thrust coefficient_opt', P_c)
+
+    m_list.append(m_dot)
+    p_list.append(P_c)
+    T_list.append(T)
+    r_list.append(r_rate)
+    prev_I = I_list[-1]
+    I_list.append(prev_I + (T * dt))
+
+    d_port += 2 * r_rate * dt
 
 
+t_list = np.arange(0, dt*len(p_list), dt)
+I_list = I_list[1:]
 
+plt.plot(t_list, p_list, label='Chamber pressure')
+plt.xlabel("Time [s]")
+plt.ylabel("Pressure [Pa]")
+plt.grid()
+plt.legend()
+plt.show()
+
+plt.plot(t_list, m_list, label='Mass flow')
+plt.xlabel("Time [s]")
+plt.ylabel("Mass flow [kg/s]")
+plt.grid()
+plt.legend()
+plt.show()
+
+plt.plot(t_list, T_list, label='Thrust')
+plt.xlabel("Time [s]")
+plt.ylabel("Thrust [N]")
+plt.grid()
+plt.legend()
+plt.show()
+
+plt.plot(t_list, I_list, label='Total impulse')
+plt.xlabel("Time [s]")
+plt.ylabel("Total impulse [Ns]")
+plt.grid()
+plt.legend()
+plt.show()
+
+plt.plot(t_list, r_list, label='Regression rate')
+plt.xlabel("Time [s]")
+plt.ylabel("Regression rate [m/s]")
+plt.grid()
+plt.legend()
+plt.show()
--- Read_Data.py
+++ Read_Data.py
@@ -30,14 +30,47 @@ def read_datafile():
                     data[params_dict['States'][i - i_next_data]] = np.float(line.strip())
 
                 if 26 + 1 <= i - i_next_data < 26 + 1 + 5:
-                    data[params_dict['Optimum'][i - i_next_data - (26 + 1)]] = np.float(line.strip())
+                    data[params_dict['Optimum'][i - i_next_data - (26 + 1)] + '_opt'] = np.float(line.strip())
 
                 if 26 + 1 + 5 + 1 <= i - i_next_data < 26 + 1 + 5 + 1 + 4:
-                    data[params_dict['Vacuum'][i - i_next_data - (26 + 1 + 5 + 1)]] = np.float(line.strip())
+                    data[params_dict['Vacuum'][i - i_next_data - (26 + 1 + 5 + 1)] + '_vac'] = np.float(line.strip())
 
     return params_dict, data_dict
 
 
+def reform(data_dict):
+    new_data_dict = data_dict[1.5]
+
+    for param in new_data_dict.keys():
+        new_data_dict[param] = [data[param] for data in data_dict.values()]
+
+    return new_data_dict
+
+
+def linearize(param_str, p):
+    data_dict = reform(read_datafile()[1])
+
+    p /= 1000000
+    p_lst = data_dict['Pressure']
+
+    i = 0
+    while p_lst[i] < p:
+        i += 1
+
+    dif = p - p_lst[i-1]
+    step = p_lst[i] - p_lst[i-1]
+
+    smaller = data_dict[param_str][i-1]
+    bigger  = data_dict[param_str][i]
+
+    return smaller + (bigger - smaller) * (dif / step)
+
+
 if __name__ == '__main__':
-    print(read_datafile()[0])
-    print(read_datafile()[1])
+    # print(read_datafile()[0])
+    # print(read_datafile()[1])
+
+    new_data = reform(read_datafile()[1])
+    print(new_data)
+
+    print(linearize('Temperature', 1.0 * 10**6))
