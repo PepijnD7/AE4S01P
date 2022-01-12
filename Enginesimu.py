d3278d626a76e8132ac42a59acfd984f58c2a551
PepijnD7 <57628178+PepijnD7@users.noreply.github.com>
2022-01-12 17:34:32 +0100

    Added chamber fill-up and cleaned up the code

--- Enginesimu.py
+++ Enginesimu.py
@@ -10,9 +10,8 @@ from Read_Provided_data import plot_given_data
 given = True
 
 # Propellant data
-a = 0.005132
 n = 0.222
-Pref = 1*10**6
+a = 0.005132 * (10 ** (-6)) ** n
 P_a = 101325
 rho_p = 1720.49
 m_p = 0.758
@@ -33,14 +32,14 @@ m_p_given = 741.8 / 1000
 alpha_given = 12 * np.pi / 180
 rho_p_given = 1786.5
 
-const = [d_port, d_out, l_p, alphast, eps, a, n, P_a, Pref, m_p, rho_p]
-conII = [d_port, d_out, l_p, alphaII, eps, a, n, P_a, Pref, m_p, rho_p]
-const_given = [d_port_given, d_out_given, l_p_given, alpha_given, eps, a, n, P_a, Pref, m_p_given, rho_p_given]
+const = [d_port, d_out, l_p, alphast, eps, a, n, P_a, m_p, rho_p]
+conII = [d_port, d_out, l_p, alphaII, eps, a, n, P_a, m_p, rho_p]
+const_given = [d_port_given, d_out_given, l_p_given, alpha_given, eps, a, n, P_a, m_p_given, rho_p_given]
 
 
 # Regression Rate
 def regrate(P, a, n):
-    r = a * (P/Pref)**n
+    r = a * P**n
     return r
 
 
@@ -59,118 +58,24 @@ def FindPratio(Gamma, eps):
     return Guess
 
 
-P_list = np.arange(1, 500)*10**6
-r_list = regrate(P_list, a, n)
-
-
-# Propellant mass variation
-V_p = l_p * ((d_out/2)**2 - (d_port/2)**2) * np.pi
-rho_p = m_p/V_p
-
-
-def burnsurf(r, l, d_i, d_o, t):
-    d_ilist = np.ones((len(t)))*d_i
-    d_list = d_ilist + 2*r*t
-    d_list[d_list>=d_o] = d_o
-    S_list = np.pi * l * d_list
-    V_list = l * ((d_o/2)**2 - (d_list/2)**2) * np.pi
-    m_list = V_list * rho_p
-    return d_list, S_list, V_list, m_list
-
-
-# Simulation
-
-# plt.subplot(1, 3, 1)
-# plt.plot(P_list, r_list*10**3, label='Regression rate')
-# plt.xlabel("Chamber Pressure (Pa)")
-# plt.ylabel("Regression rate (mm/s)")
-# plt.grid()
-# plt.legend()
-# plt.show()
-
-r_c = r_list[P_list == 100*10**6]
-t_list = np.arange(0, 10, 0.1)
-d_list, S_list, V_list, m_list = burnsurf(r_c, l_p, d_port, d_out, t_list)
-d_reg = d_list-d_port
-
-# fig, ax1 = plt.subplots()
-# plt.subplot(1, 3, 2)
-# plt.xlabel("Distance regressed (mm)")
-# plt.ylabel("Propellant mass (kg)")
-# plt.grid()
-# plt.plot(d_reg*10**3, m_list, label='Propellant mass')
-# plt.legend()
-
-# ax2 = ax1.twinx()
-
-# plt.subplot(1, 3, 3)
-# plt.xlabel("Distance regressed (mm)")
-# plt.ylabel("Burn surface (m^2)")
-# plt.plot(d_reg*10**3, S_list, label='Burn surface')
-# # fig.tight_layout()
-# plt.legend()
-# plt.grid()
-# plt.show()
-
-
-# Initial conditions
-# P_c = 5 * Pref
-# S = np.pi * l_p * d_port
-# A_t = (d_t ** 2 * np.pi / 4)
-# a *= (10 ** (-6)) ** n
-#
-# for _ in range(10):
-#     c_star = linearize('Characteristic velocity_opt', P_c)
-#     # print('c_star: ', c_star)
-#
-#     P_c = (c_star * rho_P * a * S / A_t)**(1 / (1-n))
-#     # print('P_c: ', P_c)
-#
-# a = 0.005132
-# r = regrate(P_c, a, n)
-# m_dot = r * S * rho_P
-#
-# c_star = linearize('Characteristic velocity_opt', P_c)
-# Gamma = linearize('Gamma',P_c)
-# C_f0 = linearize('Thrust coefficient_opt', P_c)
-# C_f = C_f0 * DivLoss(alpha) + (FindPratio(Gamma,eps) + P_a / P_c) * eps
-# T = C_f * P_c * A_t
-
-# print(P_c, r, m_dot, T, Gamma, C_f0, C_f)
-
-
 # Simulation
 def Simulation(con):
     g0 = 9.81
     olde = 0
-    d_port, d_out, l_p, alpha, eps, a, n, P_a, Pref, m_p, T_a = con
+    d_port, d_out, l_p, alpha, eps, a, n, P_a, m_p, rho_p = con
 
+    # INITIAL CONDITIONS and CHAMBER FILL:
+
+    # Parameters that do not change:
+    A_t = (d_t ** 2 * np.pi / 4)
     V_p = l_p * ((d_out/2)**2 - (d_port/2)**2) * np.pi
     rho_p = m_p/V_p
-
-    # Initial conditions
-    P_c = 5 * Pref
-    S = np.pi * l_p * d_port
-    A_t = (d_t ** 2 * np.pi / 4)
-    a *= (10 ** (-6)) ** n
-
-    for _ in range(10):
-        c_star = linearize('Characteristic velocity_opt', P_c)
-        P_c = (c_star * rho_p * a * S / A_t)**(1 / (1-n))
-
-    a = 0.005132
-    r = regrate(P_c, a, n)
-    m_dot = r * S * rho_p
-
-    c_star = linearize('Characteristic velocity_opt', P_c)
-    Gamma = linearize('Gamma', P_c)
-    C_f0 = linearize('Thrust coefficient_opt', P_c)
-    C_f = C_f0 * DivLoss(alpha) + (FindPratio(Gamma, eps) + P_a / P_c) * eps
-    T = C_f * P_c * A_t
-
-    # BURN LOOP
     dt = 0.1
 
+    # Parameters that will change:
+    V_c = l_p * (d_port/2)**2 * np.pi
+    P_c = P_a
+    S = np.pi * l_p * d_port
     m_list = []
     Isp_list = []
     p_list = []
@@ -179,14 +84,53 @@ def Simulation(con):
     I_list = [0]
     pepa_list = []
 
+    m_out = 0
+    m_in = regrate(P_c, a, n) * rho_p * S
+
+    while m_in-m_out>0.1:
+        S = np.pi * l_p * d_port
+        Gamma = linearize('Gamma', P_c)
+        vdk = Kerckhove(Gamma)
+        c_star = linearize('Characteristic velocity_opt', P_c)
+        C_f0 = linearize('Thrust coefficient_opt', P_c)
+
+        r = regrate(P_c, a, n)
+        m_in = r* rho_p * S
+        m_out = c_star * A_t * P_c
+        dpcdt = vdk**2/V_c * c_star**2 * (rho_p) * m_in - m_out  # Add gas density
+        C_f = C_f0 * DivLoss(alpha) + (FindPratio(Gamma, eps) - P_a / P_c) * eps
+        T = C_f * P_c * A_t
+        Isp = T / g0 / m_out
+        m_list.append(m_out)
+        Isp_list.append(Isp)
+        p_list.append(P_c)
+        pepa_list.append(P_c * FindPratio(Gamma, eps) / P_a)
+        T_list.append(T)
+        r_list.append(r)
+        prev_I = I_list[-1]
+        I_list.append(prev_I + (T * dt))
+
+        d_port+= r*dt
+        P_c += dpcdt*dt
+
+    # for _ in range(10):
+    #     c_star = linearize('Characteristic velocity_opt', P_c)
+    #     P_c = (c_star * rho_p * a * S / A_t)**(1 / (1-n))
+    # r = regrate(P_c, a, n)
+    # m_dot = r * S * rho_p
+    #
+    c_star = linearize('Characteristic velocity_opt', P_c)
+    Gamma = linearize('Gamma', P_c)
+    C_f0 = linearize('Thrust coefficient_opt', P_c)
+    C_f = C_f0 * DivLoss(alpha) + (FindPratio(Gamma, eps) + P_a / P_c) * eps
+
+    # BURN LOOP
+
     while d_port <= d_out:
         S_burn = d_port * np.pi * l_p
-
-        a *= (10 ** (-6)) ** n
         P_c = (c_star * rho_p * a * S_burn / A_t) ** (1 / (1 - n))
-        T_c = linearize("Temperature",P_c)
-
-        a = 0.005132 * np.exp(olde * (T_a - 273.15))
+        T_c = linearize("Temperature", P_c)
+        a *= np.exp(olde * (T_a - 273.15))
         # a = (1.55*10**(-5) * (T_a - 273.15) + 4.47*10**(-3))
         # print(T_c,a)
         r_rate = regrate(P_c, a, n)
@@ -199,6 +143,7 @@ def Simulation(con):
         T = C_f * P_c * A_t
         Isp = T / g0 / m_dot
         # print(Isp - linearize("Specific impulse (by mass)", P_c))
+
         m_list.append(m_dot)
         Isp_list.append(Isp)
         p_list.append(P_c)
@@ -212,7 +157,8 @@ def Simulation(con):
     t_list = np.arange(0, dt*len(p_list), dt)
     I_list = I_list[1:]
 
-    return np.array(t_list), np.array(p_list), np.array(I_list), np.array(m_list), np.array(T_list), np.array(r_list), np.array(Isp_list), np.array(pepa_list)
+    return np.array(t_list), np.array(p_list), np.array(I_list), np.array(m_list), np.array(T_list), \
+           np.array(r_list), np.array(Isp_list), np.array(pepa_list)
 
 
 t_st, p_st, I_st, m_st, T_st, r_st, Isp_st, pepa_st = Simulation(const)
